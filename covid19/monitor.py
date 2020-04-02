62e035cca34381701d6fe55a5c0b160317937a5f
Stefano Ubbiali <subbiali@phys.ethz.ch>
2020-04-02 16:04:42 +0200

    Implement MonitorComposite.

--- covid19/monitor.py
+++ covid19/monitor.py
@@ -18,12 +18,12 @@ class Monitor:
         self.loader_fcts = loader_functions
         self.drawers = drawers
         self.interactive = interactive
-        self.figure_properties = {} if figure_properties is None else figure_properties
-        self.axes_properties = {} if axes_properties is None else axes_properties
+        self.figure_properties = figure_properties or {}
+        self.axes_properties = axes_properties or {}
         self._figure = None
 
     @property
-    def figure(self) -> plt.Figure:
+    def figure(self):
         self.set_figure()
         return self._figure
 
@@ -41,7 +41,7 @@ class Monitor:
                 key: value
                 for key, value in self.axes_properties.items()
                 if key not in self.figure_properties
-            }
+            },
         )
 
         # if needed, clean the canvas
@@ -56,6 +56,80 @@ class Monitor:
         # set axes properties
         if self.axes_properties != {}:
             plot_utils.set_axes_properties(out_ax, **self.axes_properties)
+
+        # if figure is not provided, set figure properties
+        if fig is None and self.figure_properties != {}:
+            plot_utils.set_figure_properties(out_fig, **self.figure_properties)
+
+        # save
+        if not (save_dest is None or save_dest == ""):
+            _, ext = os.path.splitext(save_dest)
+            plt.savefig(save_dest, format=ext[1:], dpi=1000)
+
+        # show
+        if fig is None:
+            if self.interactive:
+                out_fig.canvas.draw()
+                plt.show(block=False)
+            elif show:
+                plt.show()
+
+        return out_fig, out_ax
+
+    def set_figure(self, fig=None):
+        if fig is not None:
+            self._figure = None
+            return
+
+        fontsize = self.figure_properties.get("fontsize", 12)
+        figsize = self.figure_properties.get("figsize", (7, 7))
+
+        if self.interactive:
+            plt.ion()
+            if self._figure is not None:
+                rcParams["font.size"] = fontsize
+                self._figure = plt.figure(figsize=figsize)
+        else:
+            plt.ioff()
+            rcParams["font.size"] = fontsize
+            self._figure = (
+                plt.figure(figsize=figsize) if self._figure is None else self._figure
+            )
+
+
+class MonitorComposite:
+    def __init__(self, slaves, nrows, ncols, interactive=False, figure_properties=None):
+        self.slaves = slaves
+        self.nrows = nrows
+        self.ncols = ncols
+        assert len(slaves) == nrows * ncols
+        self.interactive = interactive
+        self.figure_properties = figure_properties or {}
+        self._figure = None
+
+    @property
+    def figure(self):
+        self.set_figure()
+        return self._figure
+
+    def run(self, fig=None, ax=None, save_dest=None, show=False):
+        # set the private _figure attribute
+        self.set_figure(fig)
+
+        for index, slave in enumerate(self.slaves):
+            # retrieve figure and axes
+            out_fig, out_ax = plot_utils.get_figure_and_axes(
+                fig,
+                ax,
+                default_fig=self._figure,
+                nrows=self.nrows,
+                ncols=self.ncols,
+                index=index + 1,
+                **self.figure_properties,
+            )
+
+            # invoke the slaves
+            slave.run(fig=out_fig, ax=out_ax, show=False)
 
         # if figure is not provided, set figure properties
         if fig is None and self.figure_properties != {}:
